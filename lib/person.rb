71c505f73faa113cde05c4c5dfc9412f35766c64
Kevin Diesenberg <kdiesenb@gmail.com>
2022-12-14 12:22:04 -0500

    def clean? need to review this but i got the self part right.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,45 @@
 # your code goes here
+require "pry"
+
+class Person
+    attr_reader :name
+    # reader only lets you read it.  this can't be changed later
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        # upon init the name is the only thing i want to be able to pass in and the rest of the @'s are able to change later on    newName.bank_account = 60
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=(value)
+        @happiness = if value >= 10
+            10
+        elsif value <= 0
+            0
+        else
+            value
+        end
+                # @happiness = num.clamp(0, 10) i like this!
+    end
+
+    def hygiene=(value)
+        @hygiene = value.clamp(0, 10)
+    end
+
+    def happy?
+        self.happiness > 7 ? true : false
+    end
+
+    def clean?
+        self.hygiene > 7 ? true : false
+    end
+
+end
+
+
+
+
+# binding.pry
